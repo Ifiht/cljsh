2ff018e47fd9c66efd1df05fb5ad1fda98d898b6
Peter Pan <peter@never.lan>
2021-07-09 16:32:01 +0000

    building minimal repl

--- cljsh/src/cljsh/core.clj
+++ cljsh/src/cljsh/core.clj
@@ -1,6 +1,12 @@
 (ns cljsh.core
   (:gen-class))
 
+(defn repl-prompt
+  "Default :prompt hook for repl"
+  []
+  (printf "%s=> " (ns-name *ns*)))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
