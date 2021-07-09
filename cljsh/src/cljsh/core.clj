520020cd32819d621a8d100e44e1f9cc39cbeeb9
Peter Pan <peter@never.lan>
2021-07-09 16:39:11 +0000

    minimal repl funcs, build failing

--- cljsh/src/cljsh/core.clj
+++ cljsh/src/cljsh/core.clj
@@ -1,3 +1,7 @@
+;;========================| CLJSH CORE |========================;;
+;; Reference for the REPL implementation taken from clojure core,
+;; specifically clojure.main, as described here:
+;; https://github.com/clojure/clojure/blob/master/src/clj/clojure/main.clj
 (ns cljsh.core
   (:gen-class))
 
@@ -6,8 +10,106 @@
   []
   (printf "%s=> " (ns-name *ns*)))
 
+(defn repl
+  "Generic, reusable, read-eval-print loop. By default, reads from *in*,
+  writes to *out*, and prints exception summaries to *err*. If you use the
+  default :read hook, *in* must either be an instance of
+  LineNumberingPushbackReader or duplicate its behavior of both supporting
+  .unread and collapsing CR, LF, and CRLF into a single \\newline. Options
+  are sequential keyword-value pairs. Available options and their defaults:
+     - :init, function of no arguments, initialization hook called with
+       bindings for set!-able vars in place.
+       default: #()
+     - :need-prompt, function of no arguments, called before each
+       read-eval-print except the first, the user will be prompted if it
+       returns true.
+       default: (if (instance? LineNumberingPushbackReader *in*)
+                  #(.atLineStart *in*)
+                  #(identity true))
+     - :prompt, function of no arguments, prompts for more input.
+       default: repl-prompt
+     - :flush, function of no arguments, flushes output
+       default: flush
+     - :read, function of two arguments, reads from *in*:
+         - returns its first argument to request a fresh prompt
+           - depending on need-prompt, this may cause the repl to prompt
+             before reading again
+         - returns its second argument to request an exit from the repl
+         - else returns the next object read from the input stream
+       default: repl-read
+     - :eval, function of one argument, returns the evaluation of its
+       argument
+       default: eval
+     - :print, function of one argument, prints its argument to the output
+       default: prn
+     - :caught, function of one argument, a throwable, called when
+       read, eval, or print throws an exception or error
+       default: repl-caught"
+  [& options]
+  (let [cl (.getContextClassLoader (Thread/currentThread))]
+    (.setContextClassLoader (Thread/currentThread) (clojure.lang.DynamicClassLoader. cl)))
+  (let [{:keys [init need-prompt prompt flush read eval print caught]
+         :or {init        #()
+              need-prompt (if (instance? LineNumberingPushbackReader *in*)
+                            #(.atLineStart ^LineNumberingPushbackReader *in*)
+                            #(identity true))
+              prompt      repl-prompt
+              flush       flush
+              read        repl-read
+              eval        eval
+              print       prn
+              caught      repl-caught}}
+        (apply hash-map options)
+        request-prompt (Object.)
+        request-exit (Object.)
+        read-eval-print
+        (fn []
+          (try
+            (let [read-eval *read-eval*
+                  input (try
+                          (with-read-known (read request-prompt request-exit))
+                          (catch LispReader$ReaderException e
+                            (throw (ex-info nil {:clojure.error/phase :read-source} e))))]
+             (or (#{request-prompt request-exit} input)
+                 (let [value (binding [*read-eval* read-eval] (eval input))]
+                   (set! *3 *2)
+                   (set! *2 *1)
+                   (set! *1 value)
+                   (try
+                     (print value)
+                     (catch Throwable e
+                       (throw (ex-info nil {:clojure.error/phase :print-eval-result} e)))))))
+           (catch Throwable e
+             (caught e)
+             (set! *e e))))]
+    (with-bindings
+     (try
+      (init)
+      (catch Throwable e
+        (caught e)
+        (set! *e e)))
+     (prompt)
+     (flush)
+     (loop []
+       (when-not 
+       	 (try (identical? (read-eval-print) request-exit)
+	  (catch Throwable e
+	   (caught e)
+	   (set! *e e)
+	   nil))
+         (when (need-prompt)
+           (prompt)
+           (flush))
+         (recur))))))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main ;;repl-opt
+  "Start a repl with args and inits. Print greeting if no eval options were
+  present"
+  [[_ & args] inits]
+  (when-not (some #(= eval-opt (init-dispatch (first %))) inits)
+    (println "Clojure" (clojure-version)))
+  (repl :init (fn []
+                (initialize args inits)
+                (apply require repl-requires)))
+  (prn)
+  (System/exit 0))
