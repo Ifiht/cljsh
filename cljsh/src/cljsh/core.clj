d0fd0973c421118ce1954d9e91381528137a4c62
Peter Pan <peter@never.lan>
2021-07-14 20:20:39 +0000

    build failing again

--- cljsh/src/cljsh/core.clj
+++ cljsh/src/cljsh/core.clj
@@ -4,137 +4,13 @@
 ;; Reference for the REPL implementation taken from clojure core,
 ;; specifically clojure.main, as described here:
 ;; https://github.com/clojure/clojure/blob/master/src/clj/clojure/main.clj
-(ns ^{:doc "Core file & functions for minimal repl."
-       :author "ifiht"}
-  cljsh.core
+(ns ^{:doc "Core file & functions for minimal repl." :author "ifiht"} cljsh.core
   (:gen-class)
-  (:refer-clojure :exclude [with-bindings])
-  (:require [clojure.spec.alpha :as spec])
-  (:use [clojure.main :only (ex-triage)]
-        [clojure.main :only (err->msg)]
-        [clojure.main :only (ex-str)])
-  (:import (java.io StringReader BufferedWriter FileWriter)
-           (java.nio.file Files)
-           (java.nio.file.attribute FileAttribute)
-           (clojure.lang Compiler Compiler$CompilerException
-                         LineNumberingPushbackReader RT LispReader$ReaderException)))
+  (:require [clojure.main :as clojure-main])
+  (:import [org.jline.reader Completer ParsedLine LineReader]
+           (org.jline.reader.impl LineReaderImpl)))
 
 ;;+++++++++++++++++++++++++++++| MINIMAL REPL CODE |++++++++++++++++++++++++++++++++;;
-(defmacro with-bindings
-  "Executes body in the context of thread-local bindings for several vars
-  that often need to be set!: *ns* *warn-on-reflection* *math-context*
-  *print-meta* *print-length* *print-level* *compile-path*
-  *command-line-args* *1 *2 *3 *e"
-  [& body]
-  `(binding [*ns* *ns*
-             *warn-on-reflection* *warn-on-reflection*
-             *math-context* *math-context*
-             *print-meta* *print-meta*
-             *print-length* *print-length*
-             *print-level* *print-level*
-             *print-namespace-maps* true
-             *data-readers* *data-readers*
-             *default-data-reader-fn* *default-data-reader-fn*
-             *compile-path* (System/getProperty "clojure.compile.path" "classes")
-             *command-line-args* *command-line-args*
-             *unchecked-math* *unchecked-math*
-             *assert* *assert*
-             clojure.spec.alpha/*explain-out* clojure.spec.alpha/*explain-out*
-             *1 nil
-             *2 nil
-             *3 nil
-             *e nil]
-     ~@body))
-
-(defn repl-prompt
-  "Default :prompt hook for repl"
-  []
-  (printf "%s=> " (ns-name *ns*)))
-
-(def ^{:doc "A sequence of lib specs that are applied to `require`
-by default when a new command-line REPL is started."} repl-requires
-  '[[clojure.repl :refer (source apropos dir pst doc find-doc)]
-    [clojure.java.javadoc :refer (javadoc)]
-    [clojure.pprint :refer (pp pprint)]])
-
-(defmacro with-read-known
-  "Evaluates body with *read-eval* set to a \"known\" value,
-   i.e. substituting true for :unknown if necessary."
-  [& body]
-  `(binding [*read-eval* (if (= :unknown *read-eval*) true *read-eval*)]
-     ~@body))
-
-(defn repl-caught
-  "Default :caught hook for repl"
-  [e]
-  (binding [*out* *err*]
-    (print (err->msg e))
-    (flush)))
-
-(defn skip-whitespace
-  "Skips whitespace characters on stream s. Returns :line-start, :stream-end,
-  or :body to indicate the relative location of the next character on s.
-  Interprets comma as whitespace and semicolon as comment to end of line.
-  Does not interpret #! as comment to end of line because only one
-  character of lookahead is available. The stream must either be an
-  instance of LineNumberingPushbackReader or duplicate its behavior of both
-  supporting .unread and collapsing all of CR, LF, and CRLF to a single
-  \\newline."
-  [s]
-  (loop [c (.read s)]
-    (cond
-     (= c (int \newline)) :line-start
-     (= c -1) :stream-end
-     (= c (int \;)) (do (.readLine s) :line-start)
-     (or (Character/isWhitespace (char c)) (= c (int \,))) (recur (.read s))
-     :else (do (.unread s c) :body))))
-
-(defn renumbering-read
-  "Reads from reader, which must be a LineNumberingPushbackReader, while capturing
-  the read string. If the read is successful, reset the line number and re-read.
-  The line number on re-read is the passed line-number unless :line or
-  :clojure.core/eval-file meta are explicitly set on the read value."
-  {:added "1.10"}
-  ([opts ^LineNumberingPushbackReader reader line-number]
-   (let [pre-line (.getLineNumber reader)
-         [pre-read s] (read+string opts reader)
-         {:keys [clojure.core/eval-file line]} (meta pre-read)
-         re-reader (doto (LineNumberingPushbackReader. (StringReader. s))
-                     (.setLineNumber (if (and line (or eval-file (not= pre-line line))) line line-number)))]
-     (read opts re-reader))))
-
-(defn skip-if-eol
-  "If the next character on stream s is a newline, skips it, otherwise
-  leaves the stream untouched. Returns :line-start, :stream-end, or :body
-  to indicate the relative location of the next character on s. The stream
-  must either be an instance of LineNumberingPushbackReader or duplicate
-  its behavior of both supporting .unread and collapsing all of CR, LF, and
-  CRLF to a single \\newline."
-  [s]
-  (let [c (.read s)]
-    (cond
-     (= c (int \newline)) :line-start
-     (= c -1) :stream-end
-     :else (do (.unread s c) :body))))
-
-(defn repl-read
-  "Default :read hook for repl. Reads from *in* which must either be an
-  instance of LineNumberingPushbackReader or duplicate its behavior of both
-  supporting .unread and collapsing all of CR, LF, and CRLF into a single
-  \\newline. repl-read:
-    - skips whitespace, then
-      - returns request-prompt on start of line, or
-      - returns request-exit on end of stream, or
-      - reads an object from the input stream, then
-        - skips the next input character if it's end of line, then
-        - returns the object."
-  [request-prompt request-exit]
-  (or ({:line-start request-prompt :stream-end request-exit}
-       (skip-whitespace *in*))
-      (let [input (renumbering-read {:read-cond :allow} *in* 1)]
-        (skip-if-eol *in*)
-        input)))
-
 (defn repl
   "Generic, reusable, read-eval-print loop. By default, reads from *in*,
   writes to *out*, and prints exception summaries to *err*. If you use the
@@ -226,23 +102,11 @@ by default when a new command-line REPL is started."} repl-requires
            (prompt)
            (flush))
          (recur))))))
-
-(defn- repl-init ;; "defn-" provides a non-public version of "defn"
-  "Start a repl without any args, print version"
-  []
-  (println "Clojure" (clojure-version))
-  (repl :init (fn []
-                (apply require repl-requires)))
-  (prn)
-  (System/exit 0))
 ;;+++++++++++++++++++++++++++++| END REPL CODE |++++++++++++++++++++++++++++++++++++;;
 
 ;;+++++++++++++++++++++++++++++| BEGIN CLJSH CODE |+++++++++++++++++++++++++++++++++;;
-(defn -main
-  "Main function, program entry point"
-  [& args]
-  (try
-    (repl-init)
-    (catch Throwable t
-      (println "caught exception %s" t)
-      (System/exit 1))))
+(defn -main "Main function, program entry point" [& args]
+  (if (= args '("--version"))
+    (prn {:clojure (clojure-version)})
+    (with-redefs [clojure-main/repl repl]
+      (apply clojure-main/main args))))
