933320cf14258611b756a7d78096bae0f02b18bb
Peter Pan <peter@never.lan>
2021-07-22 18:29:49 +0000

    tested first dependent function :use fix

--- src/cljsh/core.clj
+++ src/cljsh/core.clj
@@ -10,7 +10,8 @@
   (:gen-class)
   (:require [clojure.main :as clojure-main]
             [clojure.string :as str]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn])
+  (:use     [clojure.main :only [skip-whitespace renumbering-read skip-if-eol]]))
 
 ;;+++++++++++++++++++++++++++++| MINIMAL REPL CODE |++++++++++++++++++++++++++++++++;;
 (defn repl-prompt
@@ -28,6 +29,23 @@
   [s]
   (reduce + (map * (vec (map edn/read-string (str/split s #"\.")))
     [100 10 1])))
+
+(defn repl-read
+  "Default :read hook for repl. Reads from *in* which must either be an instance of 
+  LineNumberingPushbackReader or duplicate its behavior of both supporting .unread and 
+  collapsing all of CR, LF, and CRLF into a single \\newline. repl-read:
+    - skips whitespace, then
+      - returns request-prompt on start of line, or
+      - returns request-exit on end of stream, or
+      - reads an object from the input stream, then
+        - skips the next input character if it's end of line, then
+        - returns the object."
+  [request-prompt request-exit]
+  (or ({:line-start request-prompt :stream-end request-exit}
+       (skip-whitespace *in*))
+      (let [input (renumbering-read {:read-cond :allow} *in* 1)]
+        (skip-if-eol *in*)
+        input)))
 ;;+++++++++++++++++++++++++++++| END REPL CODE |++++++++++++++++++++++++++++++++++++;;
 
 ;;+++++++++++++++++++++++++++++| BEGIN CLJSH CODE |+++++++++++++++++++++++++++++++++;;
@@ -35,4 +53,5 @@
   (if (= args '("--version"))
     (prn {:clojure (clojure-version)})
     (with-redefs [clojure-main/repl-prompt repl-prompt]
+                 [clojure-main/repl-read repl-read]
       (apply clojure-main/main args))))
